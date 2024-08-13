f5e1c3762f1e49f9266aa38b37e1f2b94f454c3e
Ttxman <seps.ladislav@seznam.cz>
2024-08-13 23:19:20 +0200

    csharpification of c ad h files namespaces classes

---
+++ src/vga_palette.h.cs
@@ -0,0 +1,14 @@
+#ifndef VGA_PALETTE_H
+#define VGA_PALETTE_H
+
+using static Opentyrian2000.palette;
+
+namespace Opentyrian2000;
+public partial class __filename__
+{
+    extern Palette vga_palette;
+
+#endif // VGA_PALETTE_H
+
+
+}
